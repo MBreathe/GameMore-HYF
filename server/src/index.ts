eb520d7b6e8e4ad4e15543e9f1a1d287d61de71b
Ilia Bubnov <makebreathe1224@gmail.com>
2025-07-09 10:35:08 +0200

    moved assets folder to root

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,8 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+// app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://game-more-hyf.vercel.app", credentials: true }))
 
 app.get("/", (_: Request, res: Response) => {
   res.send("Server is running");
